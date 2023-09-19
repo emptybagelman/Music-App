074bac5246224da8e6ce0da9d28ef0cb2b3bd8e0
emptybagelman <samuelmcgin@aol.com>
2023-09-19 22:17:57 +0100

    uhhh form

--- src/pages/Installations/index.jsx
+++ src/pages/Installations/index.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React,{ useState,useEffect,useRef } from 'react'
+import "./style.css"
 
 const Installations = () => {
+
+  const [inputText,setInputText] = useState("")
+  const [sortChoice,setSortChoice] = useState("latestplayed")
+  const [toggle1,setToggle1] = useState(false)
+  const [toggle2,setToggle2] = useState(false)
+  const [toggle3,setToggle3] = useState(false)
+
+  function handleInput(e){
+    setInputText(e.target.value)
+  }
+
+  function handleSortChange(e){
+    setSortChoice(e.target.value)
+    console.log(sortChoice)
+  }
+
   return (
-    <div id='main'>Installations</div>
+    <div id='topform'>
+      <form>
+        <div id="search-input">
+          <label htmlFor="search">SEARCH</label>
+          <input name="search" type="text" placeholder='Installation name' id='searchinput' value={inputText} onChange={handleInput}/>
+        </div>
+        <div id="sort-input">
+          <label htmlFor="sort">SORT BY</label>
+          <select name="sort" id="sort-input" value={sortChoice} onChange={handleSortChange}>
+            <option value="latestplayed">Latest Played</option>
+            <option value="name">Name</option>
+          </select>
+        </div>
+        <div id="versions">
+          <label>VERSIONS</label>
+          <label htmlFor="releases">Releases</label>
+          <input type="checkbox" name="releases" id="releases-cb" value="releases"/>
+          <label htmlFor="snapshots">Snapshots</label>
+          <input type="checkbox" name="snapshots" id="snapshots-cb" value="snapshots"/>
+          <label htmlFor="modded">Modded</label>
+          <input type="checkbox" name="modded" id="modded-cb" value="modded" onChange={} />
+        </div>
+      </form>
+    </div>
   )
 }
 
