d38f9f0104ac2cdbeed1e389b0c8150b471edeab
emptybagelman <samuelmcgin@aol.com>
2023-10-24 11:53:13 +0100

    bruh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Music-App/",
   plugins: [react()],
   build: {
     rollupOptions: {
